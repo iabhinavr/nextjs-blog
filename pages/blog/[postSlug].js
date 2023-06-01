bb294609945243d9f2d296f837792aa71278744b
Abhinav R <abhinav7r@gmail.com>
2023-06-02 01:04:15 +0530

    backup password updated

--- pages/blog/[postSlug].js
+++ pages/blog/[postSlug].js
@@ -5,15 +5,22 @@ import CommentForm from "../../components/CommentForm";
 import { getPostSlugs, getSinglePost } from "../../lib/posts";
 import { getComments } from "../../lib/comments";
 import Date from "../../components/Date";
-import { comment } from "postcss";
+import { Rubik, Roboto_Slab } from '@next/font/google';
+import { getSeo } from "../../lib/seo";
+
+const rubik = Rubik({ subsets: ['latin'], display: 'swap' });
+const roboto_slab = Roboto_Slab({ subsets: ['latin'], display: 'swap' });
+
+console.log(roboto_slab);
 
 export async function getStaticProps({ params }) {
     const postData = await getSinglePost(params.postSlug);
     const {comments, commentCount} = await getComments(params.postSlug);
+    const seoData = await getSeo('post', params.postSlug);
 
     let featuredImageUrl = "https://wp.abhinavr.com/wp-content/uploads/2022/12/travel_icy-polar_022K.jpg";
 
-    if(postData.featuredImage.node.mediaDetails.sizes[0].sourceUrl) {
+    if(postData.featuredImage) {
         featuredImageUrl = postData.featuredImage.node.mediaDetails.sizes[0].sourceUrl;
     }
 
@@ -29,6 +36,7 @@ export async function getStaticProps({ params }) {
             featuredImageUrl: "url(" + featuredImageUrl + ")",
             comments,
             commentCount,
+            seoData
         },
         notFound: false,
     };
@@ -49,23 +57,46 @@ export async function getStaticPaths() {
     }
 }
 
-export default function Post({ postData, featuredImageUrl, comments, commentCount }) {
+export default function Post({ postData, featuredImageUrl, comments, commentCount, seoData }) {
     console.log(comments);
+
+    let jsonSchema = seoData.schema.raw.replace(/https:\/\/wp.abhinavr.com(?!\/wp-content\/uploads)/g, 'https://coolnomad.abhinavr.com/blog')
+
     return (
         <>
         <Head>
-            <title key={postData.slug}>{postData.title}</title>
-            <meta name="description" content={postData.excerpt} key="metadescription" />
+            
+            <title key="title">{seoData.title}</title>
+            <meta name="description" content={seoData.metaDesc} key="metadesc" />
+            
+            <meta property="og:title" content={seoData.opengraphTitle} />
+            <meta key="og-description" property="og:description" content={seoData.metaDesc} />
+            <meta property="og:url" content={seoData.opengraphUrl} />
+            <meta property="og:image" content={seoData.opengraphImage.mediaItemUrl} />
+            <meta property="og:type" content={seoData.opengraphType} />
+            <meta property="og:locale" content="en_IN" />
+            
+            <meta property="og:site_name" content={seoData.opengraphSiteName} />
+            
+            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: jsonSchema }}></script>
+
+            <style>
+                {`
+                    .post-content ul {
+                        font-family: ${rubik.style.fontFamily}
+                    }
+                `}
+            </style>
         </Head>
         <section className="bg-slate-700 bg-opacity-70 absolute w-full z-20">
             <SiteHeader className="header-single-post z-10 relative" />
         </section>
-        <article>
+        <article className={`${rubik.className} font-light`}>
             <section className="hero-area h-[60vh] min-h-[30rem] bg-no-repeat bg-cover bg-center relative" style={{backgroundImage: featuredImageUrl}}>
                 <div className="absolute inset-0 bg-slate-900 opacity-40"></div>
 
                 <div className="container mx-auto h-full flex flex-col justify-center lg:max-w-4xl">
-                    <h1 className="text-6xl text-center text-slate-100 relative z-10 py-8 mt-12">{postData.title}</h1>
+                    <h1 className={`${roboto_slab.className} text-6xl font-normal text-slate-100 relative z-10 py-8 mt-12`}>{postData.title}</h1>
 
                     <div className="pb-4 text-slate-100 z-10">
                         Posted by Abhinav, last updated on <Date dateString={postData.modified} />
